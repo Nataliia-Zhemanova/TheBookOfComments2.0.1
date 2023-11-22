b3fdec24343a423d2d2954dc13c0a7d1f0de28e9
rustamkutovoi <rustamkutovoy996@gmail.com>
2023-11-21 22:19:34 -0800

    added changes to package.json; userCreate.spec.js was created

---
+++ src/modules/user/_test/userCreate.spec.js
@@ -0,0 +1,46 @@
+const request = require('supertest')
+const {expect} = require('chai')
+const graphQlEndpoint = 'http://localhost:5000/graphql'
+
+describe('USER CREATE', () => {
+
+    describe('USER CREATE - POSITIVE', () => {
+        it('user create', (done) => {
+
+            const arg = {
+                userInput: {
+                    firstName: 'firstName',
+                    lastName: 'lastName'
+                }
+            }
+
+            const postData = {
+                query: `mutation UserCreate($userInput: UserItems) {
+  userCreate(userInput: $userInput) {
+    _id
+    firstName
+    lastName
+  }
+}`,
+                variables: arg
+            }
+            request(graphQlEndpoint)
+                .post('/')
+                .send(postData)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    const respData = res.body.data
+                    console.log("RESP BODY ===", respData)
+                    expect(respData.userCreate.firstName).to.eq('firstName')
+                    expect(respData.userCreate.lastName).to.eq('lastName')
+
+                    done()
+                })
+        })
+    });
+
+    describe('USER CREATE - NEGATIVE', () => {
+
+    });
+});
