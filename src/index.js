1007602efb998ea2f7c65c0b196785b4aebb58bc
marin <voitsekhivska.marina@gmail.com>
2023-11-28 21:02:30 +0100

    userCreate.spec.js has been added

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+    typeDefs,
+    resolvers
 })
 
 mongoose.connect(MONGODB)
-  .then(() => {
-    console.log("MongoDB Connection successful");
-    return server.listen({port: 5000})
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  })
+    .then(() => {
+        console.log("MongoDB Connection successful");
+        return server.listen({port: 5000})
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`);
+    })
