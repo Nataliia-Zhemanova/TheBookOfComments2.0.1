1d92c8642cbdb1c3439ce870dc5eb7d009fa83fe
AlanSlobodianik <slobodyanik.alan@gmail.com>
2023-11-28 21:47:40 -0600

    userGetAll

---
+++ src/modules/user/_test/userGeteAll.spec.js
@@ -0,0 +1,38 @@
+const request = require('supertest')
+const { expect } = require('chai')
+const graphQLEndpoint = 'http://localhost:5000/graphql'
+
+describe('USER GET ALL', () => {
+    describe('USER GET ALL - POSITIVE', () => {
+        it('user create', (done) => {
+            const arg = {
+                amount: null
+            }
+            const postData = {
+                query: `query UsersGetAll($amount: Int) {
+  usersGetAll(amount: $amount) {
+    _id
+    firstName
+    lastName
+  }
+}`,
+                variables: arg
+            }
+            request(graphQLEndpoint)
+                .post('/')
+                .send(postData)
+                .expect(200)
+                .end((err, res) => {
+                    if(err) return done(err);
+                    const respData = res.body.data
+                    console.log("resp body===",respData)
+                    //expect(respData.userCreate.firstName).eq('firstName1')
+                    //expect(respData.userCreate.lastName).eq('lastName1')
+                    done()
+                })
+        })
+    })
+    describe('USER GET ALL - NEGATIVE', () => {
+
+    })
+})
