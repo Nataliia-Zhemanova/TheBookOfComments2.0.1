d65ce9a48e1139db07873f079dd05ec25cff083d
rustamkutovoi <rustamkutovoy996@gmail.com>
2023-12-07 20:00:22 -0800

    query.js has been added

--- src/modules/user/_test/userCreate.spec.js
+++ src/modules/user/_test/userCreate.spec.js
@@ -1,42 +1,29 @@
 const request = require('supertest')
 const {expect} = require('chai')
-const graphQlEndpoint = 'http://localhost:5000/graphql'
 
-const { requestGql}  = require ('../../helper')
+
+const {requestGql}  = require ('../../helper')
+const { userCreateM } = require ('./query')
+const { arg } = require ('./data')
 
 describe('USER CREATE', () => {
-
     describe('USER CREATE - POSITIVE', () => {
         it('user create', (done) => {
 
-            const arg = {
-                userInput: {
-                    firstName: 'firstName',
-                    lastName: 'lastName'
-                }
-            }
-
             const postData = {
-                query: `mutation UserCreate($userInput: UserItems) {
-  userCreate(userInput: $userInput) {
-    _id
-    firstName
-    lastName
-  }
-}`,
+                query: userCreateM,
                 variables: arg
             }
             requestGql(postData)
-            // request(graphQlEndpoint)
-            //     .post('/')
-            //     .send(postData)
-                    .expect(200)
-                    .end((err, res) => {
+                .expect(200)
+                .end((err, res) => {
                     if (err) return done(err);
                     const respData = res.body.data
                     console.log("RESP BODY ===", respData)
-            //         expect(respData.userCreate.firstName).to.eq('firstName')
-            //         expect(respData.userCreate.lastName).to.eq('lastName')
+                    expect(respData.userCreate.firstName).to.eq(arg.userInput.firstName)
+                    expect(respData.userCreate.lastName).to.eq(arg.userInput.lastName)
+                    expect(respData.userCreate._id).to.be.a('string')
+                    expect(respData.userCreate._id.length).not.eq(0)
 
                     done()
                 })
