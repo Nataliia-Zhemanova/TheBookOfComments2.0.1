c3b97223159d5b561b0a6d9ba27537fd771bc5a0
Nataliia-Zhemanova <nataliia.zhemanova@gmail.com>
2023-12-12 11:55:20 +0900

    the comment has been added

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "prettier/prettier": "error",
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      extends: ["prettier"],
+    },
+  ],
+};
