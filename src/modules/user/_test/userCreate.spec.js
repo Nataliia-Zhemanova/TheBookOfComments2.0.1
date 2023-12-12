9628eed9b89bb43869815d9554c569e1e36f199d
2023Olga <borodaolga087@gmail.com>
2023-12-12 17:06:18 -0500

    userCreate  change

--- src/modules/user/_test/userCreate.spec.js
+++ src/modules/user/_test/userCreate.spec.js
@@ -1,20 +1,35 @@
+const request = require('supertest')
 const {expect} = require('chai')
-const { requestGgl, requestGql} = require ('../../helper')
-const { userCreateM} = require('./gueries')
-const { arg } = require('./data')
+const graphQLEndpoint = 'http://localhost:5000/graphql'
+
+
 describe('USER CREATE', () => {
     describe('USER CREATE - POSITIVE', () => {
-
         it('user create', (done) => {
+            const arg = {
+                userInput: {
+                    firstName: 'firstName11',
+                    lastName: 'lastName11'
+                }
+            }
             const postData = {
-                query: userCreateM,
+                query: `mutation UserCreate($userInput: UserItems) {
+  userCreate(userInput: $userInput) {
+    _id
+    firstName
+    lastName
+  }
+}`,
                 variables: arg
             }
-            requestGql(postData)
+
+            request(graphQLEndpoint)
+                .post('/')
+                .send(postData)
                 .expect(200)
                 .end((err, res) => {
                     if(err) return done(err);
-                    const respData = res.body.data
+                        const respData = res.body.data
                     console.log("RESP BODY ===", respData)
                     expect(respData.userCreate.firstName).eq('firstName11')
                     expect(respData.userCreate.lastName).eq('lastName11')
