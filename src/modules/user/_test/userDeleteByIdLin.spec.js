57d40900811c5498d21ee97f4ae0e27b2709186f
olialinnik <olia.g.linnik@gmail.com>
2023-12-15 00:12:04 -0500

    with more negatives

--- src/modules/user/_test/userDeleteByIdLin.spec.js
+++ src/modules/user/_test/userDeleteByIdLin.spec.js
@@ -185,5 +185,53 @@ describe('USER DELETE BY ID', () => {
             });
         });
     });
+    describe('USER GET BY ID - NEGATIVE 2', () => {
 
+
+        before('user delete all', (done) => {
+            User.deleteMany({})
+            return done();
+        });
+
+        before('user create', (done) => {
+            const postData = {
+                query: userCreateM,
+                variables: arg,
+            };
+
+            requestGql(postData)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    const respData = res.body.data;
+                    userId = res.body.data.userCreate._id
+                    console.log("RESP BODY ===", respData)
+                    console.log("USER ID ===", userId)
+                    //expect(respData.userCreate.firstName).eq('firstName')
+                    //expect(respData.userCreate.lastName).eq('lastName')
+                    done();
+                });
+        });
+
+        it('user delete number id', (done) => {
+            const arg = {
+                userId: 888,
+            };
+            const postData = {
+                query: userDeleteByIdM,
+                variables: arg,
+            };
+
+            requestGql(postData)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    const respData = res.body.errors[0]
+                    console.log('RESP BODY USER GET BY ID ===', respData);
+                    expect(respData.message).eq('Cast to ObjectId failed for value "888" (type string) at path "_id" for model "User"')
+                    expect(respData.extensions.code).to.eq('INTERNAL_SERVER_ERROR')
+                    done();
+                });
+        });
+    });
 })
