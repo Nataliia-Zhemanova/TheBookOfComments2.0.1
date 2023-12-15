a9a92c823750637479c56823617562b8473e3442
azamatmurzabayev <box4aza@gmail.com>
2023-12-14 20:33:07 -0600

    added negative test case to deleteByIdMur file

--- src/modules/user/_test/userDeleteByIdMur.js
+++ src/modules/user/_test/userDeleteByIdMur.js
@@ -3,6 +3,8 @@ const { requestGql } = require ('../../helper')
 const { expect } = require('chai');
 const { userCreateM, userDeleteById, userDeleteByIdM} = require('./queries')
 const {userInput} = require('./data')
+const User = require ('../User')
+const generateId = require('../../../utils/generateId')
 
 
 // Начинаем тестовый набор `USER DELETE BY ID`
@@ -10,6 +12,12 @@ describe('USER DELETE BY ID', () => {
 
     // Начинаем тестовый набор `USER GET BY ID - POSITIVE`
     describe('USER DELETE BY ID - POSITIVE', () => {
+
+        before('user delete all', (done) => {
+            User.deleteMany({});
+            return done();
+        });
+
         // Объявляем переменную: `userId`
         let userId = null;
         // Начинаем тест `user create`
@@ -72,5 +80,76 @@ describe('USER DELETE BY ID', () => {
     });
 
     // Начинаем тестовый набор `USER GET BY ID - NEGATIVE`
-    describe('USER GET BY ID - NEGATIVE', () => {});
+    describe('USER GET BY ID - NEGATIVE', () => {
+
+        before('user delete all', (done) => {
+            User.deleteMany({});
+            return done();
+        });
+
+        // Объявляем переменную: `userId`
+        let userId = null;
+        // Начинаем тест `user create`
+        it('user create', (done) => {
+            // Определяем POST-данные для создания пользователя
+            const postData = {
+                query: userCreateM,
+                variables: userInput,
+            };
+            // Выполняем POST-запрос к конечной точке GraphQL для создания пользователя
+            requestGql(postData)
+                .expect(200)
+                .end((err, res) => {
+                    // Обрабатываем ошибки
+                    if (err) {
+                        return done(err);
+                    }
+                    // Сохраняем ID созданного пользователя
+                    userId = res.body.data.userCreate._id;
+                    // Выводим в консоль тело ответа и ID пользователя
+                    console.log('RESP BODY ===', res.body);
+                    console.log('USER ID ===', userId);
+                    // Завершаем тест
+                    done();
+                });
+        });
+
+        // Начинаем тест `user delete by id`
+        it('user delete by wrong id (one symbol less)', (done) => {
+            // Определяем POST-данные для удаления пользователя по ID
+            const userDelete = {
+                userId,
+            };
+            console.log(userDelete);
+            userDelete.userId = userDelete.userId.slice(0, -1);
+            console.log(userDelete);
+            const postData = {
+                query: userDeleteByIdM,
+                variables: userDelete,
+            };
+
+            // Выполняем POST-запрос к конечной точке GraphQL для получения пользователя по ID
+            requestGql(postData)
+                .expect(200)
+                .end((err, res) => {
+
+                    // Обрабатываем ошибки
+                    if (err) {
+                        return done(err);
+                    }
+
+                    const respData = res.body.errors[0];
+                    // Выводим в консоль тело ответа
+                    console.log('RESP BODY USER DELETE BY ID ===', respData);
+
+                    // Проверяем, что результат совпадает с ожидаемым
+                    expect(respData.message).eq('Cast to ObjectId failed for value "' + userDelete.userId + '" (type string) at path "_id" for model "User"');
+
+                    // Завершаем тест
+                    done();
+                });
+        });
+    });
+
+
 });
