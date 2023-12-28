4a6d3a59f09f12ec83e534fabeda565c458433c7
RafaelIsr <146373789+RafaelIsr@users.noreply.github.com>
2023-12-28 20:41:17 +0500

    USER DELETE BY ID - NEGATIVE user delete by empty id user delete with wrong type of id field

--- src/modules/user/_test/userDeleteByIdIsr.spec.js
+++ src/modules/user/_test/userDeleteByIdIsr.spec.js
@@ -3,17 +3,17 @@ const {requestGql} = require('../../helper')
 const {userDeleteMutationById} = require('./queries')
 const {argDel} = require('./data')
 describe ('USER DELETE BY ID', () => {
-    describe ('USER DELETE BY ID - POSITIVE', () => {
+    describe('USER DELETE BY ID - POSITIVE', () => {
         it('user delete by id', (done) => {
             const postData = {
                 query: userDeleteMutationById,
-                variables:  argDel
+                variables: argDel
             }
 
             requestGql(postData)
                 .expect(200)
                 .end((err, res) => {
-                    if(err) return done(err);
+                    if (err) return done(err);
                     const respData = res.body.data
                     console.log('RESP BODY ===', respData);
                     expect(respData.userDeleteById).eq(false)
@@ -22,8 +22,43 @@ describe ('USER DELETE BY ID', () => {
         })
     })
 
-    describe ('USER DELETE BY ID - NEGATIVE', () => {
+    describe('USER DELETE BY ID - NEGATIVE', () => {
+        it('user delete by empty id', (done) => {
+            const userGet = {
+                userId: '',
+            }
+            const postData = {
+                query: userDeleteMutationById,
+                variables: userGet
+            }
+            requestGql(postData)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    const respData = res.body.data
+                    console.log('RESP BODY USER DELETE BY ID ===', respData);
+                    expect(respData.userDeleteById).eq(null)
+                    done()
+                })
+        })
+        it('user delete with wrong type of id field', (done) => {
+            const userGet = {
+                userId: 1234567890,
+            }
+            const postData = {
+                query: userDeleteMutationById,
+                variables: userGet
+            }
+            requestGql(postData)
+                .expect(200)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    const respData = res.body.data
+                    console.log('RESP BODY USER DELETE BY ID ===', respData);
+                    expect(respData.userDeleteById).eq(null)
+                    done()
+                })
+        })
 
-    } )
-} )
-
+    })
+})
