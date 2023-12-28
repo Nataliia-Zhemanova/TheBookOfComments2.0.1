e4e38a888809f08e6bffdd062af49018c70b5c8d
RafaelIsr <146373789+RafaelIsr@users.noreply.github.com>
2023-12-28 20:18:51 +0500

    USER CREATE - NEGATIVE: user create with invalid type of lastName

--- src/modules/user/_test/userCreateIsr.spec.js
+++ src/modules/user/_test/userCreateIsr.spec.js
@@ -1,48 +1,29 @@
-// const qraphQLEndpoint = 'http://localhost:5000/graphql'
-// Убрали потому что импортировали
 const {expect} = require ('chai')
 const {requestGql} = require('../../helper')
 const {userCreateMutation} = require('./queries')
 const {arg} = require('./data')
+const faker = require("faker");
 describe ('USER CREATE', () => {
-    describe ('USER CREATE - POSITIVE', () => {
+    describe('USER CREATE - POSITIVE', () => {
         it('user create', (done) => {
-            // const arg = {
-            // userInput: {
-            // firstName: 'firstName1',
-            // lastName: 'lastName1'
-            // }
-            // }
             const postData = {
                 query: userCreateMutation,
-                //   query: `mutation UserCreate($userInput: UserItems) {
-                //   userCreate(userInput: $userInput) {
-                //     _id
-                //     firstName
-                //     lastName
-                //   }
-                // }`,
-                // Убрали потому что импортировали
                 variables: arg
             }
             requestGql(postData)
-                // request(qraphQLEndpoint)
-                // .post('/')
-                // .send(postData)
-                // Убрали потому что импортировали
                 .expect(200) // supertest
                 .end((err, res) => {
                     if (err) return done(err);
                     const respData = res.body.data
                     console.log('RESP BODY ===', respData);
-                    // expect(respData.userCreate.firstName).eq('firstName1')
-                    // expect(respData.userCreate.lastName).eq('lastName1')
+                    expect(respData.userCreate.firstName).eq(arg.userInput.firstName)
+                    expect(respData.userCreate.lastName).eq(arg.userInput.lastName)
                     done()
                 })
         })
     })
-    describe ('USER CREATE - NEGATIVE', () => {
-        it('user update with empty query', (done) => {
+    describe('USER CREATE - NEGATIVE', () => {
+        it('user create with empty query', (done) => {
             const postData = {
                 query: ``,
                 variables: arg
@@ -58,5 +39,27 @@ describe ('USER CREATE', () => {
                     done()
                 })
         })
-       })
+        it('user create with invalid type of lastName', (done) => {
+            const argInvalid = {
+                userInput: {
+                    firstName: null,
+                    lastName: 123
+            }
+            }
+            const postData = {
+                query: userCreateMutation,
+                variables: argInvalid
+            }
+            requestGql(postData)
+                .expect(400)
+                .end((err, res) => {
+                    if (err) return done(err);
+                    const respData = res.body.errors[0]
+                    console.log('RESP BODY ===', respData);
+                    expect(respData.message).eq('Variable "$userInput" got invalid value 123 at "userInput.lastName"; String cannot represent a non string value: 123')
+                    expect(respData.extensions.code).eq('BAD_USER_INPUT')
+                    done()
+                })
+        })
     })
+})
